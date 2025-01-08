feb232d7cbd18a347a8441d91e3cf2aba4f2677a
vansh.shah <vanshshah174@gmail.com>
2025-01-08 13:35:39 -0500

    Database setup done with Supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='text-3xl text-center mt-10'>
-        Subscribe to My chann
+        Subscribe to My channel
     </div>
   )
 }
