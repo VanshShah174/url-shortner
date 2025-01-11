2d77ff3a1464ae4d78586a9540e9eeb04c8afa8d
vansh.shah <vanshshah174@gmail.com>
2025-01-11 12:40:40 -0500

    dynamic functionality in header portion implemented along with logout logic

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,4 +1,7 @@
-import { Input } from "./ui/input";
+import {useEffect, useState} from "react";
+import Error from "./error";
+import {Input} from "./ui/input";
+import * as Yup from "yup";
 import {
   Card,
   CardContent,
@@ -7,15 +10,11 @@ import {
   CardHeader,
   CardTitle,
 } from "./ui/card";
-import { Button } from "./ui/button";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import * as Yup from "yup";
-import Error from "./error";
-import { signup } from "@/db/apiAuth";
-import { BeatLoader } from "react-spinners";
+import {Button} from "./ui/button";
+import {useNavigate, useSearchParams} from "react-router-dom";
+import {signup} from "@/db/apiAuth";
+import {BeatLoader} from "react-spinners";
 import useFetch from "@/hooks/use-fetch";
-import { UrlState } from "@/context";
 
 const Signup = () => {
   let [searchParams] = useSearchParams();
@@ -25,26 +24,24 @@ const Signup = () => {
 
   const [errors, setErrors] = useState({});
   const [formData, setFormData] = useState({
-   name:"",
+    name: "",
     email: "",
     password: "",
     profile_pic: null,
   });
 
   const handleInputChange = (e) => {
-    const { name, value , files } = e.target;
+    const {name, value, files} = e.target;
     setFormData((prevState) => ({
       ...prevState,
-      [name]: files ? files[0] :value,
+      [name]: files ? files[0] : value,
     }));
   };
 
-  const { loading, error, fn: fnSignup, data } = useFetch(signup, formData);
-  const { fetchUser } = UrlState();
+  const {loading, error, fn: fnSignup, data} = useFetch(signup, formData);
 
   useEffect(() => {
     if (error === null && data) {
-      fetchUser();
       navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -61,30 +58,33 @@ const Signup = () => {
         password: Yup.string()
           .min(6, "Password must be at least 6 characters")
           .required("Password is required"),
-          profile_pic: Yup.mixed().required("profile pic is required"),
+        profile_pic: Yup.mixed().required("Profile picture is required"),
       });
 
-      await schema.validate(formData, { abortEarly: false });
+      await schema.validate(formData, {abortEarly: false});
       await fnSignup();
-    } catch (e) {
+    } catch (error) {
       const newErrors = {};
+      if (error?.inner) {
+        error.inner.forEach((err) => {
+          newErrors[err.path] = err.message;
+        });
 
-      e?.inner?.forEach((err) => {
-        newErrors[err.path] = err.message;
-      });
-
-      setErrors(newErrors);
+        setErrors(newErrors);
+      } else {
+        setErrors({api: error.message});
+      }
     }
   };
 
   return (
-    <Card className="bg-black text-white">
+    <Card>
       <CardHeader>
         <CardTitle>Signup</CardTitle>
         <CardDescription>
           Create a new account if you haven&rsquo;t already
         </CardDescription>
-        {error && <Error message={error.message} />}
+        {error && <Error message={error?.message} />}
       </CardHeader>
       <CardContent className="space-y-2">
         <div className="space-y-1">
@@ -92,7 +92,6 @@ const Signup = () => {
             name="name"
             type="text"
             placeholder="Enter Name"
-            className="text-black"
             onChange={handleInputChange}
           />
         </div>
@@ -102,7 +101,6 @@ const Signup = () => {
             name="email"
             type="email"
             placeholder="Enter Email"
-            className="text-black"
             onChange={handleInputChange}
           />
         </div>
@@ -112,17 +110,15 @@ const Signup = () => {
             name="password"
             type="password"
             placeholder="Enter Password"
-            className="text-black"
             onChange={handleInputChange}
           />
         </div>
         {errors.password && <Error message={errors.password} />}
         <div className="space-y-1">
-          <Input
+          <input
             name="profile_pic"
             type="file"
             accept="image/*"
-            className="text-black"
             onChange={handleInputChange}
           />
         </div>
