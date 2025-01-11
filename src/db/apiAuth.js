634785eac36cc01af9648ec51a8439b1736920df
vansh.shah <vanshshah174@gmail.com>
2025-01-11 10:51:25 -0500

    signup Api implemented and Required changes done

--- src/db/apiAuth.js
+++ src/db/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function login({ email, password }) {
     const { data, error } = await supabase.auth.signInWithPassword({
@@ -18,4 +18,25 @@ export async function getCurrentUser() {
     if (!sesssion.session) return null;
     if (error) throw new Error(error.message);
     return sesssion.session?.user;
+}
+
+export async function signup({ name, email, password, profile_pic }) {
+    const filename = `dp-${name.split(" ").join("-")}-${Math.random()}`
+    const { error: storageError } = await supabase.storage.from("profile_pic").upload(filename, profile_pic);
+    if (storageError) throw new Error(storageError.message);
+
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            data: {
+                name,
+                profile_pic: `${supabaseUrl}/storage/v1/object/public/profile_pic/${filename}`,
+            }
+        }
+    })
+
+    if (error) throw new Error(error.message);
+
+    return data;
 }
