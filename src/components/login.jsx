fab4a6d7af8a150dc1c69d118ada643132a5dead
vansh.shah <vanshshah174@gmail.com>
2025-01-11 10:19:59 -0500

    useFetch Custom hook implemented for the User authentication

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import {Input} from "./ui/input";
 import {
   Card,
   CardContent,
@@ -6,43 +6,62 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
-import { BeatLoader } from "react-spinners";
-import Error from "./error";
+} from "./ui/card";
+import {Button} from "./ui/button";
+import {useNavigate, useSearchParams} from "react-router-dom";
+import {useEffect, useState} from "react";
 import * as Yup from "yup";
+import Error from "./error";
+import {login} from "@/db/apiAuth";
+import {BeatLoader} from "react-spinners";
+import useFetch from "@/hooks/use-fetch";
+// import {UrlState} from "@/context";
 
 const Login = () => {
-  const [errors, seterrors] = useState([]);
+  let [searchParams] = useSearchParams();
+  const longLink = searchParams.get("createNew");
 
+  const navigate = useNavigate();
+
+  const [errors, setErrors] = useState({});
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target;
+    const {name, value} = e.target;
     setFormData((prevState) => ({
       ...prevState,
       [name]: value,
     }));
   };
 
+  const {loading, error, fn: fnLogin, data} = useFetch(login, formData);
+//   const {fetchUser} = UrlState();
+
+  useEffect(() => {
+    if (error === null && data) {
+      // fetchUser();
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [error, data]);
+
   const handleLogin = async () => {
-    seterrors([]);
+    setErrors([]);
     try {
-      const schema = Yup.object.shape({
+      const schema = Yup.object().shape({
         email: Yup.string()
-          .email("Invalid Email")
-          .required("Email is Required"),
+          .email("Invalid email")
+          .required("Email is required"),
         password: Yup.string()
-          .min(6, "Passwords must be at least 6 characters")
-          .required("Password is Required"),
+          .min(6, "Password must be at least 6 characters")
+          .required("Password is required"),
       });
 
-      await schema.validate(formData, { abortEarly: false });
-      // api call
+      await schema.validate(formData, {abortEarly: false});
+      await fnLogin();
     } catch (e) {
       const newErrors = {};
 
@@ -50,49 +69,48 @@ const Login = () => {
         newErrors[err.path] = err.message;
       });
 
-      seterrors(newErrors);
+      setErrors(newErrors);
     }
   };
 
   return (
-    <div>
-      <Card className="bg-black text-white">
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-          <CardDescription>
-            to your account if you already have one
-          </CardDescription>
-          <Error message={"Login Failed"} />
-        </CardHeader>
-        <CardContent className="space-y-2">
-          <div className="space-y-1">
-            <Input
-              name="email"
-              type="email"
-              placeholder="Enter Email"
-              className="text-black"
-              onChange={handleInputChange}
-            />
-            {errors.email && <Error message={"Some error"} />}
-          </div>
-          <div className="space-y-1">
-            <Input
-              name="password"
-              type="password"
-              placeholder="Enter Password"
-              className="text-black"
-              onChange={handleInputChange}
-            />
-           { errors.password && <Error message={"Some error"} />}
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button onChange={handleLogin}>
-            {true ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
-          </Button>
-        </CardFooter>
-      </Card>
-    </div>
+    <Card className="bg-black text-white">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          to your account if you already have one
+        </CardDescription>
+        {error && <Error message={error.message} />}
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="space-y-1">
+          <Input
+            name="email"
+            type="email"
+            placeholder="Enter Email"
+            className="text-black"
+            onChange={handleInputChange}
+          />
+        </div>
+        {errors.email && <Error message={errors.email} />}
+        <div className="space-y-1">
+          <Input
+            name="password"
+            type="password"
+            placeholder="Enter Password"
+            className="text-black"
+
+            onChange={handleInputChange}
+          />
+        </div>
+        {errors.password && <Error message={errors.password} />}
+      </CardContent>
+      <CardFooter>
+        <Button onClick={handleLogin}>
+          {loading ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
+        </Button>
+      </CardFooter>
+    </Card>
   );
 };
 
