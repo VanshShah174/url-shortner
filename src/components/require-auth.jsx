d524511face4ea2cbeebbb54d0cb3ba7202bf9f1
vansh.shah <vanshshah174@gmail.com>
2025-01-14 14:46:18 -0500

    Dashboard page implemented along with some changes done in another pages

--- src/components/require-auth.jsx
+++ src/components/require-auth.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+
 import {useNavigate} from "react-router-dom";
 import {useEffect} from "react";
 import {UrlState} from "@/context";
--- src/db/apiClicks.js
+++ src/db/apiClicks.js
@@ -1,13 +1,15 @@
 import supabase from "./supabase";
 
 export async function getClicksForUrls(urlIds) {
-    const { data, error } = await supabase.from('clicks').select("*").in("url_id",urlIds)
-    if (!session.session) return null;
-
+    const {data, error} = await supabase
+      .from("clicks")
+      .select("*")
+      .in("url_id", urlIds);
+  
     if (error) {
-        console.error(error.message)
-        throw new Error("Unable to load Clicks!");
+      console.error("Error fetching clicks:", error);
+      return null;
     }
-
+  
     return data;
-}
+  }
