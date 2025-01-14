c26db4138efa541a989b90be8c5ab87e9577651f
vansh.shah <vanshshah174@gmail.com>
2025-01-14 15:06:38 -0500

    url deletion logic implemented

--- src/components/link-card.jsx
+++ src/components/link-card.jsx
@@ -1,9 +1,29 @@
-import { Copy, Delete, Download } from "lucide-react";
+import { Copy, Delete, Download, Trash } from "lucide-react";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
+import useFetch from "@/hooks/use-fetch";
+import { deleteUrl } from "@/db/apiUrl";
+import { BeatLoader } from "react-spinners";
 
 const LinkCard = ({ url, fetchUrls }) => {
+
+    const downloadImage = () => {
+        const imageUrl = url?.qr;
+        const fileName = url?.title
+
+        const anchor = document.createElement("a")
+        anchor.href = imageUrl;
+        anchor.download= fileName
+
+        document.body.appendChild(anchor);
+        anchor.click();
+
+        document.body.removeChild(anchor)
+    }
+
+    const { loading : loadingDelete , fn: fnDelete }  = useFetch(deleteUrl, url?.id)
+
   return (
     <div className="flex flex-col md:flex-row gap-5 border p-4 bg-gray-900 rounded-lg">
       <img
@@ -34,11 +54,11 @@ const LinkCard = ({ url, fetchUrls }) => {
              >
                 <Copy />
             </Button>
-            <Button variant="ghost">
+            <Button variant="ghost" onClick={downloadImage}>
                 <Download />
             </Button>
-            <Button variant="ghost">
-                <Delete />
+            <Button variant="ghost" onClick={() => fnDelete().then(() => fetchUrls())}>
+               { loadingDelete ? <BeatLoader size={5} color="white" /> :   <Trash /> }
             </Button>
       </div>
     </div>
